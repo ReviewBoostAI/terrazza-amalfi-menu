2ad9af54b7bb8142c6906424e331bd6d089d6ef7
Gianluca <grandingianluca@gmail.com>
2026-07-06 00:08:28 +0200

    Aggiornamento menu

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -2,6 +2,7 @@ import "./MenuItem.css";
 import { motion } from "framer-motion";
 
 import { useLanguage } from "../context/LanguageContext";
+
 import allergens from "../data/allergenes.json";
 
 export default function MenuItem({ item }) {
@@ -14,25 +15,30 @@ export default function MenuItem({ item }) {
   return (
 
     <motion.article
+
       className="menu-item"
+
       initial={{
         opacity: 0,
         y: 15
       }}
+
       animate={{
         opacity: 1,
         y: 0
       }}
+
       whileHover={{
         y: -3
       }}
+
       transition={{
         duration: .35
       }}
+
     >
 
-      {/* FOTO DEL PIATTO */}
-
+      {/* Immagine del piatto */}
       {item.image && (
         <img
           src={item.image}
@@ -45,52 +51,72 @@ export default function MenuItem({ item }) {
 
         <div className="menu-title">
 
-          <div className="menu-text">
+          <h3>
 
-            <h3>
-              {item.name?.[language] || item.name?.it}
-            </h3>
+            {item.name?.[language] || item.name?.it}
 
-            {item.description?.[language] && (
-              <p className="menu-description">
-                {item.description[language]}
-              </p>
-            )}
-
-          </div>
+          </h3>
 
           <span className="menu-price">
+
             {item.price != null
               ? `€ ${Number(item.price).toFixed(2)}`
               : t.labels.priceOnRequest}
+
           </span>
 
         </div>
 
+        {item.description?.[language] && (
+
+          <p className="menu-description">
+
+            {item.description[language]}
+
+          </p>
+
+        )}
+
         <div className="menu-footer">
 
           {item.vegetarian && (
+
             <span className="badge veg">
+
               🥗 {t.badges.vegetarian}
+
             </span>
+
           )}
 
           {item.vegan && (
+
             <span className="badge vegan">
+
               🌱 {t.badges.vegan}
+
             </span>
+
           )}
 
           {item.spicy && (
+
             <span className="badge spicy">
+
               🌶 {t.badges.spicy}
+
             </span>
+
           )}
 
           {item.featured && (
+
             <span className="badge featured">
+
               ⭐ {t.badges.featured}
+
             </span>
+
           )}
 
         </div>
@@ -100,7 +126,9 @@ export default function MenuItem({ item }) {
           <div className="menu-allergens">
 
             <strong>
+
               {t.labels.allergens}:
+
             </strong>
 
             <ul className="allergen-list">
@@ -108,7 +136,9 @@ export default function MenuItem({ item }) {
               {allergenNames.map((name, index) => (
 
                 <li key={index}>
+
                   {name}
+
                 </li>
 
               ))}
